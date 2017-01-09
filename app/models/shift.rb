40de73c5cc96356255e6c463973089ac1326aca1
Cyril Ferte <cyril@fertec.fr>
2017-01-09 10:06:18 +0000

    add validates

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,6 @@
 class Shift < ApplicationRecord
   belongs_to :worker
+  validates :planning_id, presence: true
+  validates :worker_id, presence: true
+  validates :start_date, presence: true
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,3 +1,7 @@
 class Worker < ApplicationRecord
     has_many :shift
+     validates :first_name, presence: true
+     validates :price_per_shift, presence: true
+     validates :category, presence: true, inclusion: { in: %w(medic interne interim),
+    message: "%{value} is not a valid category" }
 end
